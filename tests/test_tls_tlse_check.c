2b5224a38fe6a15cb46685ef8652d835b111057f
Velho <jewel960@gmail.com>
2022-11-21 00:16:55 +0200

    Removing cutest.
    Added libcheck in favor of cutest.
    Cutest has not been updated in long time.
    Added new test skeleton for the libcheck.

---
+++ tests/test_tls_tlse_check.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <check.h>
+
+#include "tls_tlse.c" /* TARGET */
+
+START_TEST (TLS_LoadFile)
+{
+    ck_assert_int_eq(4, 4);
+}
+END_TEST
+
+Suite * tls_tlse_suite(void)
+{
+    Suite *s;
+    TCase *tc;
+
+    s = suite_create("Mg TLS Tlse");
+    tc = tcase_create("TLS Load File");
+
+    tcase_add_test(tc, TLS_LoadFile);
+    suite_add_tcase(s, tc);
+
+    return s;
+}
+
+int main(void)
+{
+    int num_fail = 0;
+    Suite *s;
+    SRunner *sr;
+
+    s = tls_tlse_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    num_fail = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return (num_fail == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
