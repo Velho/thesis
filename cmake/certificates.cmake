af36967b6b6ac22b4f471753f1a3093a106d4d77
Velho <jewel960@gmail.com>
2023-01-28 01:37:09 +0200

    Support for building as subdirectory.
    Project can be included and mongoose can be provided separately.
    Adding new configuration variables to fine control the build,
    excluding the different parts which are not necessary.
    Build as library is aggregating parameter to disable the
    others(mg, unit tests and examples).
    Fixed the certificate to correctly manage if calling from
    a different root directory, if the project folder name is different,
    THESIS_PROJECT_FOLDER_NAME can be used to set it correctly.

--- cmake/certificates.cmake
+++ cmake/certificates.cmake
@@ -1,4 +1,8 @@
-# Test certificates
+
+# if the thesis is added as subdirectory,
+# how to correctly resolve the copying process,
+# even if it's called from a different root folder,
+# ${THESIS_PROJECT_DIR}
 set (THESIS_TEST_CERTIFICATES
     "tests/certs/ca.pem"
     "tests/certs/cert-2048.pem"
@@ -9,14 +13,29 @@ set (THESIS_TEST_CERTIFICATES
 
 include (functions)
 
+# can be set outside of certificates to control the subdir name
+set (THESIS_PROJECT_FOLDER_NAME "thesis")
+
 # @fn thesis_copy_certificates
 # @param TARGET_NAME target name
 # Target is required as the copy command is added
 # to the POST_BUILD process.
 function (thesis_copy_certificates TARGET_NAME)
+    # build_as_library defined out of scope.
+    message ("copy_certs : ${THESIS_BUILD_AS_LIBRARY}")
+
     foreach (FILE_INPUT IN LISTS THESIS_TEST_CERTIFICATES)
-        # Remove the tests/ from the output folder naming.
-        string (REPLACE "tests/" "" FILE_CERT ${FILE_INPUT})
-        thesis_copy_build (${TARGET_NAME} ${FILE_INPUT} ${FILE_CERT})
+        set (THESIS_FILE_INPUT "${FILE_INPUT}")
+        set (THESIS_FILE_REGEX "tests/")
+
+        # append the project_folder_name if built as library.
+        if (${THESIS_BUILD_AS_LIBRARY})
+            set (THESIS_FILE_INPUT "${THESIS_PROJECT_FOLDER_NAME}/${FILE_INPUT}")
+            set (THESIS_FILE_REGEX "thesis/tests")
+        endif ()
+
+        # remove the tests/ from the output folder naming.
+        string (REPLACE ${THESIS_FILE_REGEX} "" FILE_CERT ${FILE_INPUT})
+        thesis_copy_build (${TARGET_NAME} ${THESIS_FILE_INPUT} ${FILE_CERT})
     endforeach ()
 endfunction ()
